9d32ebbc8f5df32f4c09f4d8b13fa318490bdd59
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-02 17:16:14 +0800

    add loadShellcode for test, finish TestCombine about execute shellcode.

--- combiner_test.go
+++ combiner_test.go
@@ -1,3 +1,56 @@
 //go:build windows
 
 package combiner
+
+import (
+	"runtime"
+	"syscall"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows"
+)
+
+func TestCombine(t *testing.T) {
+	x86 := []byte{
+		// xor eax, eax
+		0x31, 0xC0,
+		// add eax, 86
+		0x05, 0x86, 0x00, 0x00, 0x00,
+		// ret
+		0xC3,
+	}
+	x64 := []byte{
+		// xor rax, rax
+		0x48, 0x31, 0xC0,
+		// add rax, 64
+		0x48, 0x83, 0xC0, 0x64,
+		// ret
+		0xC3,
+	}
+	shellcode := Combine(x86, x64)
+
+	addr := loadShellcode(t, shellcode)
+	ret, _, _ := syscall.SyscallN(addr)
+	rv := int(ret)
+	switch runtime.GOARCH {
+	case "386":
+		require.Equal(t, 0x86, rv)
+	case "amd64":
+		require.Equal(t, 0x64, rv)
+	default:
+		t.Fatal("unsupported architecture")
+	}
+}
+
+func loadShellcode(t *testing.T, sc []byte) uintptr {
+	size := uintptr(len(sc))
+	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
+	mProtect := uint32(windows.PAGE_EXECUTE_READWRITE)
+	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
+	require.NoError(t, err)
+	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size)
+	copy(dst, sc)
+	return scAddr
+}
